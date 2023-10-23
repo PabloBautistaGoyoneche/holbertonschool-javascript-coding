c82c9825ca15eb60d6e413f633ce491056027f3d
PabloBautistaGoyoneche <pablo.bautista.mkt@gmail.com>
2023-10-22 20:52:02 -0500

    Code reorganization

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,42 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    let characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
+
+// Example Movie ID: 3 corresponds to "Return of the Jedi"
+const movieId = process.argv[2]; // Get movie ID from command line
+
+if (!movieId) {
+  console.log("You must provide the movie ID as an argument.");
+  process.exit(1);
+}
+
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+request(apiUrl, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const movieData = JSON.parse(body);
+
+    const characterUrls = movieData.characters;
+    let characterCount = 0;
+
+    function printCharacterName() {
+      if (characterCount < characterUrls.length) {
+        const characterUrl = characterUrls[characterCount];
+        request(characterUrl, (charError, charResponse, charBody) => {
+          if (!charError && charResponse.statusCode === 200) {
+            const characterData = JSON.parse(charBody);
+            console.log(characterData.name);
+            characterCount++;
+            printCharacterName(); // Call the function recursively for the next character
+          } else {
+            console.error('Error getting character data:', charError);
+          }
+        });
+      }
+    }
+
+    printCharacterName(); // Start printing character names
+  } else {
+    console.error('Error getting movie data:', error);
   }
 });
 
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
-      }
-    }
-  });
-}
