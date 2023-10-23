31d249fc196d4dee976f6294361db481b975b10f
PabloBautistaGoyoneche <pablo.bautista.mkt@gmail.com>
2023-10-22 20:51:47 -0500

    Code reorganization

--- javascript-web_scraping/100-starwars_characters.js
+++ javascript-web_scraping/100-starwars_characters.js
@@ -1,15 +1,32 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (!error) {
-          console.log(JSON.parse(body).name);
+
+// Example Movie ID: 3 corresponds to "Return of the Jedi"
+const movieId = process.argv[2]; // Get movie ID from command line
+
+if (!movieId) {
+  console.log("Debes proporcionar el ID de la película como argumento.");
+  process.exit(1);
+}
+
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+request(apiUrl, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const movieData = JSON.parse(body);
+
+    movieData.characters.forEach((characterUrl) => {
+      request(characterUrl, (charError, charResponse, charBody) => {
+        if (!charError && charResponse.statusCode === 200) {
+          const characterData = JSON.parse(charBody);
+          console.log(characterData.name);
+        } else {
+          console.error('Error getting character data:', charError);
         }
       });
     });
+  } else {
+    console.error('Error getting movie data:', error);
   }
 });
+
