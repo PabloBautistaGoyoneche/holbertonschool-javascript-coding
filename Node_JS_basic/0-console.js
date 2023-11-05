ea0cfeb5079996f47fe39f3b1f7b1016bf12d0e0
PabloBautistaGoyoneche <pablo.bautista.mkt@gmail.com>
2023-11-05 10:52:00 -0500

    Executing basic javascript with Node JS

---
+++ Node_JS_basic/0-console.js
@@ -0,0 +1,8 @@
+const console = require('console');
+
+function displayMessage(message) {
+    // Utiliza la función 'log' del objeto 'console' para imprimir 'message' en la salida estándar (STDOUT)
+    console.log(message);
+  }
+
+ module.exports = displayMessage;
